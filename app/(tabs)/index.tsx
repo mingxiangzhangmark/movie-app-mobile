fe8124507ac59816a9fa3206629e74ad1b3269bc
Mark.Zhang <mingxiangzhang_mark@outlook.com>
2025-05-14 02:56:01 +1000

    Add search bar to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,32 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Text, View, Image, ScrollView } from "react-native";
+import SearchBar from "../components/SearchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+
+  const router = useRouter();
   return (
     <View
-      className="flex-1 items-center justify-center "
+      className="flex-1  bg-[#030014]" 
     >
-      <Text className="text-primary text-3xl">Welcome</Text>
+      <Image source = {images.bg} className="w-full absolute z-0"/>
+      <ScrollView className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+
+          <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+            </View>
+      </ScrollView>
     
     </View>
   );
